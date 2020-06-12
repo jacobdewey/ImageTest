652ef062600b6246a482e05d5f6982f2135e7a8f
jacobdewey <66138103+jacobdewey@users.noreply.github.com>
2020-06-12 14:46:59 -0400

    Update ImageTest.ts

--- ImageTest.ts
+++ ImageTest.ts
@@ -1,8 +1,13 @@
 //% weight=100 color=#333333 icon="\uf289"
 namespace image{
 //% block
-export function giantImage(c: Image): Image{
-
+export function giantImage(c: Image=(images.createBigImage(`
+. . . . .
+. . . . .
+. . # . .
+. . . . .
+. . . . .
+`))): Image{
 return c
 
 
